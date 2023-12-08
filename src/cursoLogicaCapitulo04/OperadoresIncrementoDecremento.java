290d37af3f61c0057729993b0702686c287193c5
Emerson <emersonsdossantos@gmail.com>
2023-12-07 22:27:52 -0300

    Operadores: Operadores de incremento e decremento

---
+++ src/cursoLogicaCapitulo04/OperadoresIncrementoDecremento.java
@@ -0,0 +1,23 @@
+package cursoLogicaCapitulo04;
+
+public class OperadoresIncrementoDecremento {
+
+	public static void main(String[] args) {
+//		Integer numero = 10;
+//		System.out.println("Numero antes " + numero);
+//		Integer numero = 10;
+//		++ numero;
+//		System.out.println("Numero depois " + numero);
+		
+//		Integer numero = 10;
+//		Integer numero2 = ++numero;
+//		Integer numero2 = numero++;
+//		
+//		System.out.println("Numero: " + numero + ", Numero 2: " + numero2);
+		
+		Integer numero = 10;
+		--numero;
+		System.out.println(numero);
+	}
+
+}

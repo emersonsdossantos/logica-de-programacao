7e17d0a105c1f6dcc7c6757f85cdb246a2ab0dc4
Emerson <emersonsdossantos@gmail.com>
2023-12-19 23:43:00 -0300

    Estrutura de decisão: Utilizando o “if” encadeado

---
+++ src/cursoLogicaCapitulo05/UtilizandoIfEncadeado.java
@@ -0,0 +1,29 @@
+package cursoLogicaCapitulo05;
+
+import java.util.Scanner;
+
+public class UtilizandoIfEncadeado {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite o peso: ");
+		Double peso = scanner.nextDouble();
+		
+		Boolean pesoLeve = peso < 60;
+		Boolean pesoMedio = (peso > 60) && (peso <= 90);
+		Boolean pesoPesado =  peso > 90;
+		
+		if (pesoLeve) {
+			System.out.println("O lutador é peso Leve");
+		} else if (pesoMedio){
+			System.out.println("O lutador é peso Medio");
+		} else {
+			System.out.println("O lutador é peso Pesado");
+		}
+		
+		scanner.close();
+
+	}
+
+}

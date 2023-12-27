c2a8beac8c370904d89ea612c0ea93edad5edc7d
Emerson <emersonsdossantos@gmail.com>
2023-12-26 21:52:17 -0300

    Iteração: Iterando com o for

---
+++ src/cursoLogicaCapitulo06/IteracaoFor.java
@@ -0,0 +1,13 @@
+package cursoLogicaCapitulo06;
+
+public class IteracaoFor {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=5; i++) {
+			System.out.println(i + ": Uma frase qualquer!");
+		}
+
+	}
+
+}

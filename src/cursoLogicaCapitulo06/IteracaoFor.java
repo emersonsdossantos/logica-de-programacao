d3d1c41fd2bc0a531c5e1670ef30b73951e7da1c
Emerson <emersonsdossantos@gmail.com>
2023-12-26 22:14:39 -0300

    Iteração: Iterando com o for, usando array

--- src/cursoLogicaCapitulo06/IteracaoFor.java
+++ src/cursoLogicaCapitulo06/IteracaoFor.java
@@ -1,13 +1,23 @@
 package cursoLogicaCapitulo06;
 
+import java.util.Iterator;
+
 public class IteracaoFor {
 
 	public static void main(String[] args) {
 		
-		for (int i = 1; i <=5; i++) {
-			System.out.println(i + ": Uma frase qualquer!");
+//		for (int i = 1; i <=5; i++) {
+//			System.out.println(i + ": Uma frase qualquer!");
+//		}
+		
+		Double[] carrinhoDeCompras = new Double [] {50.0, 51.0, 52.0};
+		Double total =0.0;
+		
+		for(int i = 0; i < carrinhoDeCompras.length; i++) {
+			System.out.println("Iteração: " + i + " Total = " + total);
+			total = total + carrinhoDeCompras[i];
 		}
-
+		System.out.println("Total: " + total);
 	}
 
 }

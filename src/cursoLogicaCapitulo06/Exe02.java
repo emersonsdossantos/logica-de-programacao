1509d2831ab792ca2f1773d5553b9d564d734a0f
Emerson <emersonsdossantos@gmail.com>
2023-12-28 20:14:34 -0300

    Iteração: Exercício 02

---
+++ src/cursoLogicaCapitulo06/Exe02.java
@@ -0,0 +1,28 @@
+package cursoLogicaCapitulo06;
+
+public class Exe02 {
+
+		static final Integer DIVISOR = 3;
+
+		public static void main(String[] args) {
+			
+			Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+			
+			int i = 0;
+			while(i < numeros.length) {
+				Integer numero = numeros[i];			
+				Integer moduloporTres = numero % DIVISOR;
+				
+				if(moduloporTres.equals(0)) {
+					System.out.println("O numero " + numero + " é divisivél por " + DIVISOR);				
+				}
+				
+				i++;
+			}
+			System.out.println("Fim");
+		}
+
+
+	
+
+}

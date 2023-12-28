e6847a81aa920d796668d28e5da61ea088064774
Emerson <emersonsdossantos@gmail.com>
2023-12-28 19:44:19 -0300

    Iteração: Iterando com o while

---
+++ src/cursoLogicaCapitulo06/IteracaoWhile.java
@@ -0,0 +1,18 @@
+package cursoLogicaCapitulo06;
+
+public class IteracaoWhile {
+
+	public static void main(String[] args) {
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println(i + ": Qualquer coisa");
+//		}
+		
+		int i = 0;
+		while(i < 10) {
+			System.out.println(i + ": Qualquer coisa");
+			i++;
+		}
+		System.out.println("Fim");
+	}
+
+}

41f3a7c82aa65f2d3b61699d63cb91588b918a0c
Emerson <emersonsdossantos@gmail.com>
2023-12-28 21:56:33 -0300

    Vetores: Declarando vetores de 1 dimensão, adicionando valor do veto

--- src/cursoLogicaCapitulo07/Vetores1Dimensao.java
+++ src/cursoLogicaCapitulo07/Vetores1Dimensao.java
@@ -20,10 +20,20 @@ public class Vetores1Dimensao {
 //		
 //		scanner.close();
 		
-		String[] cardapio = new String[] {"Calabresa", "Atum", "Queijo", "Presunto"};
-		cardapio[3] = "Frango";
+//		String[] cardapio = new String[] {"Calabresa", "Atum", "Queijo", "Presunto"};
+//		cardapio[3] = "Frango";
+//		for(int i = 0; i < cardapio.length; i++) {
+//			System.out.println("["+ i +"] " + cardapio[i]);
+//		}
+		
+		String[] cardapio = new String[4];
+		cardapio[0] = "Calabresa";
+		cardapio[1] = "Atum";
+		cardapio[2] = "Queijo";
+		cardapio[3] = "Presunto";
+		
 		for(int i = 0; i < cardapio.length; i++) {
-			System.out.println("["+ i +"] " + cardapio[i]);
+			System.out.println(cardapio[i]);
 		}
 
 	}

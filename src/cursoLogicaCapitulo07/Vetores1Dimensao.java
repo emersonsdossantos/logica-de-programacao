e3cfcefecbfe05ef2745c5ddd909a771485385b1
Emerson <emersonsdossantos@gmail.com>
2023-12-28 21:29:23 -0300

    Vetores: Declarando vetores de 1 dimensão

---
+++ src/cursoLogicaCapitulo07/Vetores1Dimensao.java
@@ -0,0 +1,25 @@
+package cursoLogicaCapitulo07;
+
+import java.util.Scanner;
+
+public class Vetores1Dimensao {
+
+	public static void main(String[] args) {
+		
+		System.out.println("********Cardapio*********\n");
+		String[] cardapio = new String[] {"Calabresa", "Atum", "Queijo", "Presunto"};
+		for(int i = 0; i < cardapio.length; i++) {
+			System.out.println("["+ i +"] " + cardapio[i]);
+		}
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o numero referente ao sabor do Cardapio: ");
+		Integer saborEscolhido = scanner.nextInt();
+		
+		System.out.println("Você escolheu o sabor " + cardapio[saborEscolhido]);
+		
+		scanner.close();
+
+	}
+
+}

b4315e571f63a5c0553719e1ff6d4154b798b310
Emerson <emersonsdossantos@gmail.com>
2023-12-30 00:49:44 -0300

    Vetores: Declarando vetores de 2 dimensões

--- src/cursoLogicaCapitulo07/Vetores2Dimensoes.java
+++ src/cursoLogicaCapitulo07/Vetores2Dimensoes.java
@@ -3,14 +3,26 @@ package cursoLogicaCapitulo07;
 public class Vetores2Dimensoes {
 
 	public static void main(String[] args) {
+		
 		Double[] faturamentoJaneiro = new Double[] {1800.0, 2000.0, 1700.0};
 		Double[] faturamentoFevereiro = new Double[] {2800.0, 2500.0, 3700.0};
 		
 		Double[][]faturamentoAnual = new Double[][] {faturamentoJaneiro, faturamentoFevereiro};
 		
-		System.out.println("Faturamento do dia 1 de Janeiro: " + faturamentoAnual[0] [0]);
-		System.out.println("Faturamento do dia 3 de Fevereiro: " + faturamentoAnual[1] [2]);
-
+//		System.out.println("Faturamento do dia 1 de Janeiro: " + faturamentoAnual[0] [0]);
+//		System.out.println("Faturamento do dia 3 de Fevereiro: " + faturamentoAnual[1] [2]);
+		
+		for(int i = 0; i < faturamentoAnual.length; i++) {
+			System.out.println("Mês: " + (i+1));
+			
+			Double[] mes = faturamentoAnual[i];
+			
+			for(int y = 0; y < mes.length; y++) {
+				Double dia = mes[y];
+				System.out.println("Dia " + (y + 1) + ": " + dia);
+			}
+		}
+		System.out.println("fim");
 	}
 
 }
